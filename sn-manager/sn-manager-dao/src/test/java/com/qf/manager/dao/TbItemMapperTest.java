e648f9bd60d179d630179697fa1569a8111c2a19
xueyu <1375156056@qq.com>
2019-08-19 14:18:27 +0800

    创建骨架

---
+++ sn-manager/sn-manager-dao/src/test/java/com/qf/manager/dao/TbItemMapperTest.java
@@ -0,0 +1,22 @@
+package com.qf.manager.dao;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:spring/spring-dao-test.xml")
+public class TbItemMapperTest {
+
+    @Autowired
+    private TbItemMapper itemDao;
+
+    @Test
+    public void selectByPrimaryKey(){
+        System.out.println(itemDao.selectByPrimaryKey(605616L));
+    }
+
+
+}
